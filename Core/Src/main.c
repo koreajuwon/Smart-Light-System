1e744ce29b14ec13ba841ed7f21ae667e468da3a
koreajuwon <wndnjs6413@naver.com>
2026-06-22 13:35:40 +0900

    Test LED blinking

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,9 +96,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
